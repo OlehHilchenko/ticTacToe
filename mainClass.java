177a5a0b0e9f2f0793a3ad6a6cfd8eb6f75ea883
OlehHilchenko <50135410+OlehHilchenko@users.noreply.github.com>
2019-08-02 14:51:41 +0300

    tictactoeNew
    tictactoeNew

---
+++ mainClass.java
@@ -0,0 +1,8 @@
+package local.learn.mypets.tictactoeNew;
+
+public class mainClass {
+    public static void main(String[] args) {
+      methodClass objPlay = new methodClass();
+      objPlay.playWithPlayerOrMachine();
+    }
+}
